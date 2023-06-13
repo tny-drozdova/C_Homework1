4d086db14d85611dfde9dd12c761907c6f999eea
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-12 21:46:13 -0400

    Added homework Task 1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter the first number: ");
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);
+
+Console.Write("Enter the second number: ");
+string inputedString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} is greater number.");
+    Console.WriteLine($"{number2} is smaller number.");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"{number1} is smaller number.");
+    Console.WriteLine($"{number2} is greater number.");
+}
+else
+{
+    Console.WriteLine($"{number1} and {number2} are equal.");
+}
