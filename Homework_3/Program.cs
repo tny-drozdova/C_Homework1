72a41b86601aded04dd07155a795045fb9ef9362
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-12 22:23:38 -0400

    Added homework Task 3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter any number: ");
+string inputedString = Console.ReadLine(); // Console reads the user's input
+int number = Convert.ToInt32(inputedString);
+
+if (number %2 == 0)
+{
+    Console.WriteLine($"The number {number} is even.");
+}
+else
+{
+    Console.WriteLine($"The number {number} is odd.");
+}
